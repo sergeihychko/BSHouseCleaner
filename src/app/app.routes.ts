083c6ebc377280cfd95ad9971856144322b6d467
Serge Hychko <shychko@gmail.com>
2025-09-17 01:47:54 -0400

    Routing added. Advanced search page in progress. Date pickers need work.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import {TableComponent} from './table-component/table-component';
+import {DeleteTableComponent} from './delete-table-component/delete-table-component';
+import {SearchTableComponent} from './search-table-component/search-table-component';
+import {TableService} from './table-service';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'recent',
+    component : TableComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'delete',
+    component : DeleteTableComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'advanced',
+    component : SearchTableComponent,
+    pathMatch: 'full',
+  }
+  ,
+  {
+    path: '**',
+    redirectTo: 'recent',
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Nav} from './nav/nav';
-import {TableComponent} from './table-component/table-component';
-import {DeleteTableComponent} from './delete-table-component/delete-table-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav, TableComponent],
+  imports: [RouterOutlet, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
