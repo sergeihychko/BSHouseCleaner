7b1c931ff8ed223669a71acf508cb7d7d923bbf0
Serge Hychko <shychko@gmail.com>
2026-01-13 15:36:36 -0500

    Housecleaning to remove warnings.

--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -1,11 +1,7 @@
 import {Component, inject} from '@angular/core';
-import {App} from '../app';
-import {DecimalPipe} from '@angular/common';
-import {PrimeTemplate} from 'primeng/api';
 import {NotificationsService} from '../services/notifications';
 import {TableModule} from 'primeng/table';
-import {NzMenuDirective, NzMenuItemComponent, NzMenuModule} from 'ng-zorro-antd/menu';
-import {FeedTableService} from '../services/feed-table-service';
+import {NzMenuDirective, NzMenuItemComponent} from 'ng-zorro-antd/menu';
 import {Observable} from 'rxjs';
 import {Notificaion} from '../interface/api-interface';
 
@@ -21,20 +17,21 @@ import {Notificaion} from '../interface/api-interface';
 })
 export class Nav {
   notifications!: Observable<Notificaion[]>;
-  notificationsservice = inject(NotificationsService);
-  notificationcount: number = 0;
+  notificationsService = inject(NotificationsService);
+  notificationCount: number = 0;
+
+  constructor() {
+    console.log("MyService initializing");
+    this.getNotifications()
+  }
 
   title() {
     return "BSHouseCleaner";
   }
 
-  ngOnInit() {
-    this.getNotifications()
-  }
-
   getNotifications(){
     console.log('notifications : ', this.notifications);
-    this.notifications = this.notificationsservice.getNotificaionts();
+    this.notifications = this.notificationsService.getNotificaionts();
   }
 
 }
