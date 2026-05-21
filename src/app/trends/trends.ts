d1e6b75541ec8392e978c9127fade7610f42915c
Serge Hychko <shychko@gmail.com>
2026-05-21 13:36:48 -0400

    Changes upgrading Angular version.

--- src/app/trends/trends.ts
+++ src/app/trends/trends.ts
@@ -3,12 +3,14 @@ import {TrendsService} from '../services/trends-service';
 import {Trend} from '../interface/api-interface';
 import {PrimeTemplate} from 'primeng/api';
 import {TableModule} from 'primeng/table';
+import {TrendingFeed} from '../trending-feed/trending-feed';
 
 @Component({
   selector: 'app-trends',
   imports: [
     PrimeTemplate,
-    TableModule
+    TableModule,
+    TrendingFeed
   ],
   templateUrl: './trends.html',
   styleUrl: './trends.css',
